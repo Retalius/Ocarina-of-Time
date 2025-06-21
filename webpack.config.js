0f928a0b661ed420191ea5d30a391e1ce311c36a
Youssef Kasem <retaliuswowshiz@gmail.com>
2025-06-21 15:49:49 +0200

    Add new SVG icons and images for N64 C buttons and ocarina songs; remove old bundle.js and add new minified JS; configure TypeScript and Webpack for improved asset management

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,52 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/scripts/main.ts',
+  entry: {
+    main: {
+      import: ['./src/scripts/main.ts', './src/styles/main.scss'],
+    }
+  },
   output: {
-    path: path.resolve(__dirname, 'static', 'js'),
-    publicPath: '/static/js/',
+    path: path.resolve(__dirname, 'static'),
+    filename: 'js/[name].min.js',
     clean: true,
-    filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['.js', '.json', '.wasm', '.ts', '.tsx']
+    extensions: ['.js', '.json', '.ts', '.tsx', '.scss', '.webp', '.svg'],
   },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'], // Extract SCSS to separate files
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(webp|svg|png|jpg|jpeg|gif)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].min.css', // CSS files will be output to static/css
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/images', to: 'img' },
+        { from: 'src/fonts', to: 'fonts' }
+      ],
+    }),
+  ],
 };
